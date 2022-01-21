88c70df28a5b2a91e5d8dfc445f6c7b1782196e9
Anush Mishra <anushmishra@Anushs-MacBook-Pro.local>
2022-01-21 16:10:48 +0530

    login

---
+++ app/src/main/java/com/example/medsavvy/splashscreen.java
@@ -0,0 +1,33 @@
+package com.example.medsavvy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splashscreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(splashscreen.this, login.class);
+                //Intent is used to switch from one activity to another.
+                startActivity(i);
+                //invoke the SecondActivity.
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
